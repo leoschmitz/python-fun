73b4a743883edcb0eed8470de5c010c682ab7d41
Leonardo Augusto Schmitz-LEOAUG <leoaug@motorola.com>
2018-02-07 18:53:07 -0200

    Add quine program
    Program that writes itself (no file reading cheats).

---
+++ quine.py
@@ -0,0 +1,4 @@
+from __future__ import print_function
+c='from __future__ import print_function'
+d="print(c, chr(10), chr(99), chr(61), chr(39), c, chr(39), chr(10), chr(100), chr(61), chr(34), d, chr(34), chr(10), d, sep='')"
+print(c, chr(10), chr(99), chr(61), chr(39), c, chr(39), chr(10), chr(100), chr(61), chr(34), d, chr(34), chr(10), d, sep='')
